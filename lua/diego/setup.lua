d6e01e97c97cf5997a7a18e6debae8af308aeeb0
Dio12334 <diego.paredes.r@utec.edu.pe>
2024-08-23 17:40:36 -0500

    feat: added comments plugin and fonts autoinstall
    The plugin can be found in lua/diego/plugins/comment.lua no basic
    configuration needed.
    
    The autoinstall of fonts was added in the setup.sh file.

--- lua/diego/setup.lua
+++ lua/diego/setup.lua
@@ -25,6 +25,8 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff     = 8
 
+vim.opt.clipboard:append('unnamedplus')
+
 vim.api.nvim_create_autocmd("BufNewFile", {
     pattern = "Makefile",
     command = "0r ~/.config/nvim/templates/Makefile"
--- setup.sh
+++ setup.sh
@@ -6,6 +6,15 @@ sudo apt install luarocks
 sudo apt install ripgrep
 sudo apt install fd-find
 sudo apt install npm
+sudo apt install xclip
+
+# font del nvim
+mkdir -p ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
+unzip 0xProto.zip -d 0xProto
+cp 0xProto/*ttf ~/.local/share/fonts/
+rm -r 0xProto 0xProto.zip
+fc-cache -f -v ~/.local/share/fonts/
 
 rm -rf ~/.config/nvim
 ln -s $(pwd) ~/.config/nvim
