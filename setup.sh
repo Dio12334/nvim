330d0093aee063644ff79f10a6be9b653b902543
Dio12334 <diego.paredes.r@utec.edu.pe>
2024-08-22 21:24:35 -0500

    fix: setup.sh has correct install commands

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-snap install neovim --classic
-snap install clangd --classic
-apt install luarocks
-apt install ripgrep
-apt install fd-find
-apt install npm
+sudo snap install nvim --classic
+sudo snap install clangd --classic
+sudo apt install luarocks
+sudo apt install ripgrep
+sudo apt install fd-find
+sudo apt install npm
 
 rm -rf ~/.config/nvim
 ln -s $(pwd) ~/.config/nvim
